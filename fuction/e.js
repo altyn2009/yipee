fb7fee5525d21167c8aed879be482ac3a41ccc67
altyn <ahmetovlatynbek10@gmail.com>
2025-12-13 17:53:36 +0500

    ish

---
+++ fuction/e.js
@@ -0,0 +1,56 @@
+// 1
+function greet(){
+    console.log("Привет, мир!")
+}
+greet()
+
+// 2
+function sayHello(name){
+    console.log("Привет, ", name, "!")
+}
+sayHello("Altynbek")
+
+// 3
+function sum(one, two){
+    console.log(one+two)
+}
+sum(1, 2)
+
+// 4
+function isEven(a, b){
+    console.log(a===b)
+}
+isEven(1, 3)
+
+// 5
+function max(a, b){
+    if(a==b){
+        console.log("Числа равны")
+    }else if(a>b){
+        console.log(a)
+    }else{
+        console.log(b)
+    }
+}
+
+// 6
+function getInitials(a){
+    let g = ''
+    for(let i=0; a[i]!=' '; i += 1){
+        g=i
+    }
+    console.log(a[0] + a[g+2])
+}
+getInitials("Ahmetov Altynbek")
+
+// 7
+function square(a){
+    return a*a
+}
+function cube(a){
+    return square(a)*a
+}
+console.log(cube(3))
+
+// 8
+function sum(a, b){console.log(a+b)}
